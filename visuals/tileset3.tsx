7cc10652fe6126152c687f02fea8aff94215054a
alex <me@example.com>
2023-04-27 14:01:05 +0100

    tilesets

---
+++ visuals/tileset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset3" tilewidth="32" tileheight="32" tilecount="30" columns="6">
+ <image source="Downloads/tileset.png" trans="f7f6f5" width="192" height="160"/>
+</tileset>
